9d9305aa4a7ddbc3c6f89d5828afec6fd77d2692
Oscar Cauich <ocauich@gravitypayments.com>
2017-05-16 14:00:57 -0700

    initiated our json package

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,6 +6,7 @@
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
